f23db7f25df85aa5e4434ea8d232db14d459c670
Lori Dunford <loridk@gmail.com>
2024-05-02 20:14:37 -0400

    Add notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NotesListComponent],
+  imports: [RouterOutlet, HeaderComponent, NotesListComponent, AddNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
